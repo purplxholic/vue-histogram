fd334797e40a0b770168e4a64479440a1bb7f618
zanette <zanette_cheng@mymail.sutd.edu.sg>
2017-12-01 12:41:00 +0800

    yay

--- histogram-proj/src/components/HelloWorld.vue
+++ histogram-proj/src/components/HelloWorld.vue
@@ -1,25 +1,25 @@
 <template>
-  <!-- <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div> -->
 
-  <svg width="960" height="500"></svg>
+
+  <svg :width="width" :height="height">
+    <!-- <g v-for="bin in bins"> -->
+      <rect class ="bar" v-for="bin in bins"
+        :x="1"
+        :transform="'translate(' + x(bin.x0) + ',' + y(bin.length) + ')'"
+        :width="x(bin.x1) - x(bin.x0)-1 "
+        :height="height - y(bin.length)">
+      </rect>
+      <!-- <text
+        :dy="0.75em"
+        :y="6"
+        :x="(x(bins[0].x1) - x(bins[0].x0)) / 2"
+        :text-anchor="middle">
+      </text> -->
+
+    </g>
+
+  </svg>
+
 </template>
 
 
@@ -29,8 +29,26 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      random_data: d3.range(1000).map(d3.randomBates(10))
+      // msg: 'Welcome to Your Vue.js App',
+      random_data: d3.range(1000).map(d3.randomBates(10)),
+      width: 500,
+      height: 500
+    }
+  },
+  computed: {
+    bins () {
+      let x = this.x
+      let data = this.random_data
+      return d3.histogram().domain(x.domain()).thresholds(x.ticks(20))(data)
+    },
+    x () {
+      let width = this.width
+      return d3.scaleLinear().rangeRound([0, width])
+    },
+    y () {
+      let height = this.height
+      let bins = this.bins
+      return d3.scaleLinear().domain([0, d3.max(bins, function (d) { return d.length })]).range([height, 0])
     }
   }
 }
@@ -52,11 +70,11 @@ li {
 a {
   color: #42b983;
 }
-.bar rect {
+rect {
   fill: steelblue;
 }
 
-.bar text {
+text {
   fill: #fff;
   font: 10px sans-serif;
 }
