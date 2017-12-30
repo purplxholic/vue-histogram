44bb7d285c9ed63b2f7ddfd68670178c00ee5556
zanette <zanette_cheng@mymail.sutd.edu.sg>
2017-12-30 21:22:38 +0800

    added new files

---
+++ histogram-proj/src/components/AllCharts.vue
@@ -0,0 +1,16 @@
+<template>
+  <histogram />
+</template>
+<script>
+import Histogram from './Histogram.vue'
+import LineGraph from './LineGraph.vue'
+import ScatterPlot from './ScatterPlot.vue'
+export default {
+  name: 'AllCharts',
+  components: {
+    'histogram': Histogram,
+    'line-graph': LineGraph,
+    'scatter-plot': ScatterPlot
+  }
+}
+</script>
--- histogram-proj/src/router/index.js
+++ histogram-proj/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AllCharts from '@/components/AllCharts'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'AllCharts',
+      component: AllCharts
     }
   ]
 })
